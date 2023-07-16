485e7408a2996de9fbaccafd8b39aa70aa1387b1
harshit0571 <harshitchadha65@gmail.com>
2023-07-16 22:41:02 +0530

    table to display data added

--- client/app/page.js
+++ client/app/page.js
@@ -1,7 +1,20 @@
 "use client";
+import DataTable from "@/components/Datagrid";
 import Image from "next/image";
 
 export default function Home() {
+  const rows = [
+    { id: 1, lastName: "Snow", firstName: "Jon" },
+    { id: 2, lastName: "Lannister", firstName: "Cersei" },
+    { id: 3, lastName: "Lannister", firstName: "Jaime" },
+    { id: 4, lastName: "Stark", firstName: "Arya" },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys" },
+    { id: 6, lastName: "Melisandre", firstName: null },
+    { id: 7, lastName: "Clifford", firstName: "Ferrara" },
+    { id: 8, lastName: "Frances", firstName: "Rossini" },
+    { id: 9, lastName: "Roxie", firstName: "Harvey" },
+  ];
+
   const Send = async () => {
     const response = await fetch("/api/Send", {
       method: "POST",
@@ -10,17 +23,19 @@ export default function Home() {
     console.log(data);
   };
   return (
-    <main className="flex min-h-screen flex-col items-center  p-24">
+    <main className="flex min-h-screen flex-col items-center  p-4 sm:p-24 ">
       <div className="flex flex-col items-center text-center mb-24">
-        <h1 className="font-bold font-sans text-8xl text-red-600 m-4">
+        <h1 className="font-bold font-sans text-5xl sm:text-8xl text-red-600 m-4">
           Email-Ease
         </h1>
         <p className="text-red-400 text-2xl font-serif">
           Send bulk emails to users
         </p>
       </div>
+
+      <DataTable rows={rows} />
       <button
-        className="px-4 py-2 bg-red-600 text-white border-white rounded-lg "
+        className="px-4 mt-10 py-2 bg-red-600 text-white border-white rounded-lg "
         onClick={Send}
       >
         Send
---
+++ client/components/Datagrid.jsx
@@ -0,0 +1,42 @@
+import { DataGrid } from "@mui/x-data-grid";
+
+const columns = [
+  { field: "id", headerName: "ID", width: 130 },
+  { field: "firstName", headerName: "First name", width: 160 },
+  { field: "lastName", headerName: "Last name", width: 160 },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+  },
+];
+
+export default function DataTable({ rows }) {
+  return (
+    <div
+      style={{
+        height: 400,
+        width: "50%",
+        minWidth: "300px",
+        overflow: "scroll",
+        background: "white",
+      }}
+    >
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
+    </div>
+  );
+}
